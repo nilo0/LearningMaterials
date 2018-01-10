92e8bfd781590d9d980de7c88d33c1b4785136ba
nilo0 <nil.rahmati@gmail.com>
2018-01-10 20:20:37 +0100

    added integral to the polynomial module

--- Python/MyModules/polynomial.py
+++ Python/MyModules/polynomial.py
@@ -5,3 +5,8 @@ def calc(A, x):
 # calculating the value of the differential of a given polynomial
 def diff(A, x):
 	return sum([ai * i * x**(i-1) for (i, ai) in enumerate(A) if i != 0])
+
+# calculating the integral of the polynomial for a given range
+def integral(A, x0, x1, n):
+	dx = (float(x1) - float(x0)) / n
+	return sum([calc(A, x0 + i * dx) * dx for i in range(int(n))])
