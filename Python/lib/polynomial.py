38db8c2c68e293f729243ab094bf5bdeb14ef0e4
nilo0 <nil.rahmati@gmail.com>
2018-02-02 21:24:02 +0100

    refactored poynomial module into a class

--- Python/lib/polynomial.py
+++ Python/lib/polynomial.py
@@ -1,20 +1,20 @@
-# calculating the value of a given polynomial
-def calc(A, x):
-	return sum([ai * x**i for (i, ai) in enumerate(A)])
+class Polynomial:
+	def __init__(self, A):
+		self.A = A
 
-# calculating the value of the differential of a given polynomial
-def diff(A, x):
-	return sum([ai * i * x**(i-1) for (i, ai) in enumerate(A) if i != 0])
+	def calc(self, x):
+		return sum([ai * x**i for (i, ai) in enumerate(self.A)])
 
-# calculating the integral of the polynomial for a given range
-def integral(A, x0, x1, n):
-	dx = (float(x1) - float(x0)) / n
-	return sum([calc(A, x0 + i * dx) * dx for i in range(int(n))])
+	def diff(self, x):
+		return sum([ai * i * x**(i-1) for (i, ai) in enumerate(self.A) if i != 0])
 
-# calculating the Newton Method for a given polynomial and a precission
-def newtons_method(A, x, e):
-	if calc(A, x) <= e:
-		print "x: ", x, "f(x) = ", calc(A, x)
-		return
+	def integral(self, x0, x1, n):
+		dx = (float(x1) - float(x0)) / n
+		return sum([self.calc(x0 + i * dx) * dx for i in range(int(n))])
 
-	newtons_method(A, x - (calc(A, x) / diff(A, x)), e)
+	def newtons_method(self, x, e):
+		if self.calc(x) <= e:
+			print "x: ", x, "f(x) = ", self.calc(x)
+			return
+
+		self.newtons_method(x - (self.calc(x) / self.diff(x)), e)
